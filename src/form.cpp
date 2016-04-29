54407e22e53933c23c08685732ee561300249d8f
feilin wu <feilinwu2036@gmail.com>
2016-04-29 08:52:37 +0800

    command model/view implement

--- src/form.cpp
+++ src/form.cpp
@@ -9,18 +9,27 @@
 #include <QFileSystemModel>
 #include <QStandardItemModel>
 
+#include <QTimer>
+
 #include <QtCore/QDebug>
 
 Form::Form(QWidget *parent) :
     QWidget(parent),row(0),
+    index(0), status(false),
     ui(new Ui::Form)
 {
     ui->setupUi(this);
     model = new QStandardItemModel();
 
     initUI();
+
+    timer = new QTimer(this);
+    timer->setInterval(1000);
+
     initConnect();
-    //ui->tableWidget->setAcceptDrops(false);
+    initModel();
+
+    m_list = new QStringList;
 
 }
 
@@ -37,18 +46,23 @@ void Form::about()
 
 void Form::initUI()
 {
-    QStringList header;
-    header << tr("指令名称") << tr("指令内容");
-    model->setHorizontalHeaderLabels(header);
-    ui->tableView->setModel(model);
+
 }
 
 void Form::initConnect()
 {
     //connect(ui->emergencyButton, SIGNAL(clicked()), SLOT(emergencyStop()));
     //connect(ui->emergencyButton, SIGNAL(clicked()), SIGNAL(getData(QByteArray)));
+    connect(timer, SIGNAL(timeout()), this, SLOT(step()));
 }
 
+void Form::initModel()
+{
+    model = new QStandardItemModel(0, 2, this);
+    model->setHeaderData(0, Qt::Horizontal, tr("指令类型"));
+    model->setHeaderData(1, Qt::Horizontal, tr("指令内容"));
+
+}
 
 
 void Form::receiveData(const QByteArray &data)
@@ -69,69 +83,76 @@ void Form::dropEvent(QDropEvent *event)
 
 void Form::on_absAddBtn_clicked()
 {
-    //tableWidget增加一行
-    //指令流增加两条指令（绝对位移，目标速度）
-//    QTableWidgetItem *newItem = new QTableWidgetItem();
-//    newItem->setIcon(QIcon(QPixmap(":/images/clear.png")));
-//    ui->tableWidget->setItem(1,1,newItem);
 
+    int pos = ui->absMoveDistance->text().toInt();
+    int spd = ui->absRunSpd->value();
 
-    //ui->tableWidget->setHorizontalHeaderLabels(header);
-    //ui->tableWidget->setItem(0,0, new QTableWidgetItem("John"));
-    //ui->tableWidget->setItem(1,0, new QTableWidgetItem("Mars"));
-    //ui->tableWidget->setItem(2,0, new QTableWidgetItem("Hero"));
+    quint8 *qpos = convert(pos);
+    quint8 *qspd = convert(spd);
+    QByteArray qa;
+    quint8 qPos[10] = {0x02,0x00,ABS_MOVE_CMD,0x01,0x00,qpos[0],qpos[1],qpos[2],qpos[3],0x00};
+    quint8 qSpd[10] = {0x02,0x00,SETMOVESPCMD,0x01,0x00,qspd[0],qspd[1],qspd[2],qspd[3],0x00};
 
-    //ui->tableWidget->setModel(model);
-    //model->setHorizontalHeaderLabels(header);
-
-    quint32 pos = ui->absMoveDistance->text().toUInt();
-    quint32 spd = ui->absRunSpd->value();
+    QByteArray qa1 = array(qPos, 4);
+    QByteArray qa2 = array(qSpd, 4);
+    qa.append(qa1).append(qa2);
+    qDebug() << qa.toHex();
+    m_list->append(qa.toHex());
 
     QStringList list;
     list << tr("绝对运动指令") << QString(tr("绝对运行距离 %1mm， 运行速度档位 %2\%")).arg(pos).arg(spd);
-    QStandardItem *item = new QStandardItem(list.at(0));
-    model->setItem(row,0, item);
+    //m_list->append(tr("绝对运行指令+目标速度设置指令"));
 
-    QStandardItem *item1 = new QStandardItem(list.at(1));
-    //item->setText(QString::number(spd));
-    model->setItem(row,1,item1);
+
+    model->insertRow(row, QModelIndex());
+    model->setData(model->index(row, 0), list.value(0));
+    model->setData(model->index(row, 1), list.value(1));
 
     row++;
 
     ui->tableView->setModel(model);
+    //qDebug() << "row = " + row;
 }
 
 void Form::on_relaAddBtn_clicked()
 {
     int pos = ui->relMoveDistance->text().toInt();
-    quint32 spd = ui->relRunSpd->value();
+    int spd = ui->relRunSpd->value();
+
+    quint8 *qpos = convert(pos);
+    quint8 *qspd = convert(spd);
+    QByteArray qa;
+    quint8 qPos[10] = {0x02,0x00,REL_MOVE_CMD,0x01,0x00,qpos[0],qpos[1],qpos[2],qpos[3],0x00};
+    quint8 qSpd[10] = {0x02,0x00,SETMOVESPCMD,0x01,0x00,qspd[0],qspd[1],qspd[2],qspd[3],0x00};
+    QByteArray qa1 = array(qPos, 4);
+    QByteArray qa2 = array(qSpd, 4);
+    qa.append(qa1).append(qa2);
+    qDebug() << qa.toHex();
+    m_list->append(qa.toHex());
 
     QStringList list;
     list << tr("相对运动指令") << QString(tr("相对运行距离 %1mm， 运行速度档位 %2\%")).arg(pos).arg(spd);
-    //QStandardItem *item = new QStandardItem(list.at(0));
-    //model->setItem(row,0, item);
-    //QStandardItem *item1 = new QStandardItem(list.at(1));
-    //model->setItem(row,1,item1);
-    QList<QStandardItem*> items;
-    //items->append(new QStandardItem(list.at(0)));
-    //items->append(new QStandardItem(list.at(1)));
-    items << new QStandardItem(list.at(0)) << new QStandardItem(list.at(1));
-    model->appendRow(items);
+    //m_list->append(tr("相对运行指令+目标速度设置指令"));
+
+    model->insertRow(row, QModelIndex());
+    model->setData(model->index(row, 0), list.value(0));
+    model->setData(model->index(row, 1), list.value(1));
 
     row++;
 
     ui->tableView->setModel(model);
-
+    //qDebug() << "row = " + row;
 }
 
 void Form::on_resetBtn_clicked()
 {
-    row = 0;
-    model->clear();
-    QStringList header;
-    header << tr("指令名称") << tr("指令内容");
-    model->setHorizontalHeaderLabels(header);
+    m_list->clear(); //清空指令序列
+
+    model->removeRows(0, row, QModelIndex());
     ui->tableView->setModel(model);
+    row = 0;
+    index = 0;
+    //qDebug() << "row = " + row;
 }
 
 void Form::on_autoAct_clicked()
@@ -142,14 +163,67 @@ void Form::on_autoAct_clicked()
     //增加定时器触发事件
     //增加串口数据发送和接收事件
 
+    timer->start();
+    status = true;
+
 }
 
 void Form::on_deleteBtn_clicked()
 {
-    //ui->tableView->selectRow();
-    //删除选中行
-    if (!row--)
+    row--;
+    model->removeRow(row, QModelIndex());
+
+    m_list->removeAt(row); //删除追加指令
+    if (row < 0)
         row = 0;
-    model->removeRow(row);
-    qDebug() << "row value is " << row;
+    //qDebug() << "row = " << row;
+}
+
+
+void Form::on_stepAct_clicked()
+{
+    if (index == row)
+        index = 0;
+
+    qDebug() << m_list->value(index);
+    ui->tableView->selectRow(index);
+    index++;
+
+
+}
+
+quint8* Form::convert(int data)
+{
+    static quint8 temp[4];
+    //for(int i=0; i<size; i++)
+    temp[0] = data;
+    temp[1] = data >> 8;
+    temp[2] = data >> 16;
+    temp[3] = data >> 24;
+
+    return temp;
+}
+
+QByteArray Form::array(quint8 *data, int size)
+{
+    QByteArray array;
+    for(int i=0; i<size; i++)
+        array[i] = data[i];
+
+    return array;
+}
+
+void Form::step()
+{
+    if (index == row)
+        index = 0;
+    ui->tableView->selectRow(index);
+    qDebug() << m_list->value(index);
+    index++;
+}
+
+void Form::on_stopAct_clicked()
+{
+    timer->stop();
+    status = false;
 }
--- src/form.h
+++ src/form.h
@@ -3,38 +3,14 @@
 
 #include <QWidget>
 
-#define ABS_MOVE_CMD 0x01 //绝对运动
-#define REL_MOVE_CMD 0x02 //相对运动
-#define SETMOVESPCMD 0x03 //目标速度
-
-#define OPERATEPARAM 0x31 //操作参数命令 1)++  2)-- 3)query
-#define COMPAREPARAM 0x32 //比较参数值 1)> 2)= 3<
-
-
-#define SETACCDURCMD 0xa1 //设置加速时间(单位ms)
-#define SETDECDURCMD 0xa2 //设置减速时间(单位ms)
-#define SETPOSMAXPOS 0xa3 //设置正向最大允许位置(单位mm)
-#define SETNEGMAXPOS 0xa4 //设置负向最大允许位置(单位mm)
-#define SETPLUSCTRLT 0xa5 //设置脉冲控制方式,0x01--外部脉冲控制; 其他:内部脉冲控制
-#define SETCODELOGDI 0xa6 //设置编码器逻辑方向 1-正向 0-负向
-#define SETMOTDIVCMD 0xb1 //设置电机细分 1,2,4,8,32,64,128,256
-#define SETCUGEARCMD 0xb2 //设置电机电流档位 1-32
-#define SETREFVOLCMD 0xb3 //设置参考电压 0 - 低 1 - 高
-#define SETSMARTCURR 0xb4 //设置智能电流控制值 0 - 设置电流值1/2; 1 - 设置电流值1/4
-
-#define BATCHCONFCMD 0xca //批量配置
-#define BATCHCMDNUMB 0x0a //批量配置参数命令数目
-#define CMDDATANUM   0x0a //命令字节数
-#define DATANUMBER   0x04 //数据字节数
-
-#define EQUIPID      0x02 //设备ID
-
 
 namespace Ui {
 class Form;
 }
 
 class QStandardItemModel;
+class QAbstractItemModel;
+
 class Form : public QWidget
 {
     Q_OBJECT
@@ -64,6 +40,8 @@ private slots:
 
     void on_deleteBtn_clicked();
 
+    void on_stepAct_clicked();
+
 protected:
     void dragEnterEvent(QDragEnterEvent *event); //拖动事件
     void dropEvent(QDropEvent *event); //放置事件
@@ -71,16 +49,30 @@ protected:
 signals:
     void sendData(const QByteArray &data);
 
+private slots:
+    void step();
+
+    void on_stopAct_clicked();
+
 private:
     void initUI();
     void initConnect();
+    void initModel();
+
+    quint8* convert(int data);
+    QByteArray array(quint8*, int size);
 
 private:
     Ui::Form *ui;
     QByteArray echo; //串口返回数据
     //QStandardItemModel *header;
-    QStandardItemModel *model;
-    int row;
+    //QStandardItemModel *model;
+    QAbstractItemModel *model;
+    QStringList *m_list;
+    int row;   //行位置
+    int index; //指令序列位置
+    QTimer *timer;
+    bool status;
 };
 
 #endif // FORM_H
