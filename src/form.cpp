68a4caaff40c5960ac5f5c61a61b64189b689189
feilin wu <feilinwu2036@gmail.com>
2016-05-06 08:08:33 +0800

    主界面改版

--- src/form.cpp
+++ src/form.cpp
@@ -23,25 +23,32 @@ Form::Form(QWidget *parent) :
 
     initUI();
 
-    timer = new QTimer(this);
-    timer->setInterval(1000);
+    ford_timer = new QTimer(this);
+    ford_timer->setInterval(1000); //间隔1秒
+
+    back_timer = new QTimer(this);
+    back_timer->setInterval(1000); //间隔1秒
 
     initConnect();
     initModel();
 
-    m_list = new QStringList;
+    //m_list = new QStringList;
+    cmd_list = new QList<QByteArray>;
 
 }
 
 Form::~Form()
 {
     delete ui;
+    delete ford_timer;
+    delete back_timer;
+    delete model;
 }
 
 void Form::about()
 {
     QMessageBox::about(this, tr("控制器配置程序"),
-                       tr("/***********开发版 开发者@吴飞林*************/"));
+                       tr("/***********测试版*************/"));
 }
 
 void Form::initUI()
@@ -51,9 +58,8 @@ void Form::initUI()
 
 void Form::initConnect()
 {
-    //connect(ui->emergencyButton, SIGNAL(clicked()), SLOT(emergencyStop()));
-    //connect(ui->emergencyButton, SIGNAL(clicked()), SIGNAL(getData(QByteArray)));
-    connect(timer, SIGNAL(timeout()), this, SLOT(step()));
+    connect(ford_timer, SIGNAL(timeout()), this, SLOT(forward()));
+    connect(back_timer, SIGNAL(timeout()), this, SLOT(backward()));
 }
 
 void Form::initModel()
@@ -61,14 +67,13 @@ void Form::initModel()
     model = new QStandardItemModel(0, 2, this);
     model->setHeaderData(0, Qt::Horizontal, tr("指令类型"));
     model->setHeaderData(1, Qt::Horizontal, tr("指令内容"));
-
+    ui->tableView->setModel(model);
 }
 
 
 void Form::receiveData(const QByteArray &data)
 {
     echo = data;
-    qDebug() << echo.toHex().toUpper();
 }
 
 void Form::dragEnterEvent(QDragEnterEvent *event)
@@ -87,21 +92,26 @@ void Form::on_absAddBtn_clicked()
     int pos = ui->absMoveDistance->text().toInt();
     int spd = ui->absRunSpd->value();
 
-    quint8 *qpos = convert(pos);
-    quint8 *qspd = convert(spd);
-    QByteArray qa;
+    quint8 qpos[4];
+    convert(qpos, pos);
+    quint8 qspd[4];
+    convert(qspd, spd);
+
+    //QByteArray qa;
     quint8 qPos[10] = {0x02,0x00,ABS_MOVE_CMD,0x01,0x00,qpos[0],qpos[1],qpos[2],qpos[3],0x00};
     quint8 qSpd[10] = {0x02,0x00,SETMOVESPCMD,0x01,0x00,qspd[0],qspd[1],qspd[2],qspd[3],0x00};
 
-    QByteArray qa1 = array(qPos, 4);
-    QByteArray qa2 = array(qSpd, 4);
-    qa.append(qa1).append(qa2);
-    qDebug() << qa.toHex();
-    m_list->append(qa.toHex());
+    QByteArray qa1, qa2;
+    array2qa(qa1, qPos, 10);
+    array2qa(qa2, qSpd, 10);
+    //qa.append(qa1).append(qa2);
+    //qDebug() << qa.toHex();
+    //m_list->append(qa1.append(qa2).toHex());
+    //m_list->append(qa2.toHex());
+    cmd_list->append(qa1.append(qa2));
 
     QStringList list;
     list << tr("绝对运动指令") << QString(tr("绝对运行距离 %1mm， 运行速度档位 %2\%")).arg(pos).arg(spd);
-    //m_list->append(tr("绝对运行指令+目标速度设置指令"));
 
 
     model->insertRow(row, QModelIndex());
@@ -111,7 +121,6 @@ void Form::on_absAddBtn_clicked()
     row++;
 
     ui->tableView->setModel(model);
-    //qDebug() << "row = " + row;
 }
 
 void Form::on_relaAddBtn_clicked()
@@ -119,20 +128,25 @@ void Form::on_relaAddBtn_clicked()
     int pos = ui->relMoveDistance->text().toInt();
     int spd = ui->relRunSpd->value();
 
-    quint8 *qpos = convert(pos);
-    quint8 *qspd = convert(spd);
-    QByteArray qa;
+    quint8 qpos[4];
+    convert(qpos, pos);
+    quint8 qspd[4];
+    convert(qspd, spd);
+
+    //QByteArray qa;
     quint8 qPos[10] = {0x02,0x00,REL_MOVE_CMD,0x01,0x00,qpos[0],qpos[1],qpos[2],qpos[3],0x00};
     quint8 qSpd[10] = {0x02,0x00,SETMOVESPCMD,0x01,0x00,qspd[0],qspd[1],qspd[2],qspd[3],0x00};
-    QByteArray qa1 = array(qPos, 4);
-    QByteArray qa2 = array(qSpd, 4);
-    qa.append(qa1).append(qa2);
-    qDebug() << qa.toHex();
-    m_list->append(qa.toHex());
+    QByteArray qa1, qa2;
+    array2qa(qa1, qPos, 10);
+    array2qa(qa2, qSpd, 10);
+    //qa.append(qa1).append(qa2);
+    //qDebug() << qa.toHex();
+    //m_list->append(qa1.append(qa2).toHex());
+    //m_list->append(qa2.toHex());
+    cmd_list->append(qa1.append(qa2));
 
     QStringList list;
     list << tr("相对运动指令") << QString(tr("相对运行距离 %1mm， 运行速度档位 %2\%")).arg(pos).arg(spd);
-    //m_list->append(tr("相对运行指令+目标速度设置指令"));
 
     model->insertRow(row, QModelIndex());
     model->setData(model->index(row, 0), list.value(0));
@@ -141,89 +155,117 @@ void Form::on_relaAddBtn_clicked()
     row++;
 
     ui->tableView->setModel(model);
-    //qDebug() << "row = " + row;
 }
 
 void Form::on_resetBtn_clicked()
 {
-    m_list->clear(); //清空指令序列
+    //m_list->clear(); //清空指令序列
+    cmd_list->clear();
 
     model->removeRows(0, row, QModelIndex());
     ui->tableView->setModel(model);
     row = 0;
     index = 0;
-    //qDebug() << "row = " + row;
-}
-
-void Form::on_autoAct_clicked()
-{
-    //指令自动执行
-    //指令是打包发，还是一条一条发
-    //指令model需要改写
-    //增加定时器触发事件
-    //增加串口数据发送和接收事件
-
-    timer->start();
-    status = true;
-
 }
 
 void Form::on_deleteBtn_clicked()
 {
-    row--;
-    model->removeRow(row, QModelIndex());
-
-    m_list->removeAt(row); //删除追加指令
     if (row < 0)
         row = 0;
-    //qDebug() << "row = " << row;
+
+    model->removeRow(row, QModelIndex());
+
+    //m_list->removeAt(row); //删除追加指令
+    cmd_list->removeAt(row);
+
+    row--;
 }
 
 
 void Form::on_stepAct_clicked()
 {
-    if (index == row)
-        index = 0;
+    if (index > row-1)
+        index = row-1;
+    qDebug() << "row = " << row << " index = " << index;
 
-    qDebug() << m_list->value(index);
+    emit sendData(cmd_list->at(index));
+
+    //qDebug() << m_list->value(index);
     ui->tableView->selectRow(index);
     index++;
-
-
 }
 
-quint8* Form::convert(int data)
+void Form::convert(quint8 *buf, int data)
 {
-    static quint8 temp[4];
-    //for(int i=0; i<size; i++)
-    temp[0] = data;
-    temp[1] = data >> 8;
-    temp[2] = data >> 16;
-    temp[3] = data >> 24;
-
-    return temp;
+    buf[0] = data;
+    buf[1] = data >> 8;
+    buf[2] = data >> 16;
+    buf[3] = data >> 24;
 }
 
-QByteArray Form::array(quint8 *data, int size)
+void Form::array2qa(QByteArray &data, quint8 *buf, int size)
 {
-    QByteArray array;
     for(int i=0; i<size; i++)
-        array[i] = data[i];
-
-    return array;
-}
-
-void Form::step()
-{
-    if (index == row)
-        index = 0;
-    ui->tableView->selectRow(index);
-    qDebug() << m_list->value(index);
-    index++;
+        data[i] = buf[i];
 }
 
 void Form::on_stopAct_clicked()
 {
-    timer->stop();
+    ford_timer->stop();
+    back_timer->stop();
     status = false;
+}
+
+void Form::run()
+{
+
+}
+
+void Form::backward()
+{
+    if(index < 0) {
+        index = 0;
+        back_timer->stop();
+        status = false;
+    } else if(index > row-1) {
+        index = row-1;
+    }
+    qDebug() << "row = " << row << " index = " << index;
+
+    emit sendData(cmd_list->at(index));
+
+    ui->tableView->selectRow(index);
+    //qDebug() << m_list->value(index);
+    index--;
+}
+
+void Form::on_backwardAct_clicked()
+{
+    back_timer->start();
+    status = true;
+}
+
+void Form::forward()
+{
+    if(index > row-1) {
+        index = row-1;
+        ford_timer->stop();
+        status = false;
+    } else if(index < 0) {
+        index = 0;
+    }
+    qDebug() << "row = " << row << " index = " << index;
+
+    emit sendData(cmd_list->at(index));
+
+    ui->tableView->selectRow(index);
+    //qDebug() << m_list->value(index);
+    index++;
+}
+
+void Form::on_forwardAct_clicked()
+{
+
+    ford_timer->start();
+    status = true;
 }
--- src/form.h
+++ src/form.h
@@ -22,25 +22,17 @@ public:
 
 public slots:
     void about();
+    void run();
 
 private slots: 
     //绝对运动
-    //void on_absoluteMove_clicked();
-
-    //void on_relativeMove_clicked();
-
-    //void on_stopMove_clicked();
     void on_absAddBtn_clicked();
 
     void on_relaAddBtn_clicked();
 
     void on_resetBtn_clicked();
 
-    void on_autoAct_clicked();
-
     void on_deleteBtn_clicked();
-
-    void on_stepAct_clicked();
 
 protected:
     void dragEnterEvent(QDragEnterEvent *event); //拖动事件
@@ -50,17 +42,24 @@ signals:
     void sendData(const QByteArray &data);
 
 private slots:
-    void step();
+    void forward();
+    void backward();
 
     void on_stopAct_clicked();
+
+    void on_backwardAct_clicked(); //逆向回退
+
+    void on_forwardAct_clicked(); //正向前进
+
+    void on_stepAct_clicked(); //单步运行
 
 private:
     void initUI();
     void initConnect();
     void initModel();
 
-    quint8* convert(int data);
-    QByteArray array(quint8*, int size);
+    void convert(quint8 *buf, int data); //int -> quint8[4]
+    void array2qa(QByteArray &data, quint8 *buf, int size); //quint8[4] -> QByteArray
 
 private:
     Ui::Form *ui;
@@ -68,11 +67,13 @@ private:
     //QStandardItemModel *header;
     //QStandardItemModel *model;
     QAbstractItemModel *model;
-    QStringList *m_list;
+    //QStringList *m_list;
     int row;   //行位置
     int index; //指令序列位置
-    QTimer *timer;
+    QTimer *ford_timer; //正向计时器
+    QTimer *back_timer; //逆向计时器
     bool status;
+    QList<QByteArray> *cmd_list; //指令列表
 };
 
 #endif // FORM_H
